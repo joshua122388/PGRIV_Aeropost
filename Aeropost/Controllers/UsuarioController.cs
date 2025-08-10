90ff51a1d57852579b45fa0531ba8aa18c1d1e29
joshua122388 <certificationvm@gmail.com>
2025-08-10 10:50:31 -0600

    se agrega controlador para objeto Usuario, en donde se llama a la funcion login desde Service.cs para validacion de login.

---
+++ Aeropost/Controllers/UsuarioController.cs
@@ -0,0 +1,95 @@
+﻿using Aeropost.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aeropost.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private Service services;
+
+        public UsuarioController()
+        {
+            this.services = new Service();
+        }
+
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string usuario, string password)
+        {
+            try
+            {
+                var usuarioLogueado = services.login(usuario, password);
+                // pendiente de definir
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
