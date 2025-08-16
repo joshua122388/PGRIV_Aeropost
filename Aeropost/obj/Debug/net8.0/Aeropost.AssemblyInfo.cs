bc0ca02461bf5a6dd3ada9da5610b30eaf1027f5
joshua122388 <certificationvm@gmail.com>
2025-08-16 14:09:35 -0600

    ocultar tabs

--- Aeropost/obj/Debug/net8.0/Aeropost.AssemblyInfo.cs
+++ Aeropost/obj/Debug/net8.0/Aeropost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aeropost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3204173c7e9dd1629890c4768f1b9a9bdebb99c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5dd2f233410e3c669cd15197e3e5dfa733a053b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aeropost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aeropost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
