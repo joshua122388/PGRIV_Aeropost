63230e05619dfd169347772c22049adc3abcca29
sebastianchennavarro <sebastian.cheng@ulatina.net>
2025-08-17 00:49:25 -0600

    creacion de la view factura y aplique el css del template

--- Aeropost/obj/Debug/net8.0/Aeropost.AssemblyInfo.cs
+++ Aeropost/obj/Debug/net8.0/Aeropost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aeropost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea68453e95a3408fe02851fea7fe8287fbd6e59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557c5c1cd6cdf0970b227f30658389e2827490a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aeropost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aeropost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
